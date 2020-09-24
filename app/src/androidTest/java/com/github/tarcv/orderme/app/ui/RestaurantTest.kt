e44c096f6033e8b4b2a0c78764187b93c25c450b
Sergey Buzovsky <sbuzovsky@gmail.com>
2020-09-23 18:43:08 -0700

    Added test for OceanSeafood image visibility verification.

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/RestaurantTest.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/RestaurantTest.kt
@@ -3,9 +3,7 @@ package com.github.tarcv.orderme.app.ui
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
@@ -32,5 +30,13 @@ class RestaurantTest {
         onView(withText("Ocean Seafood")).perform(click())
 
         onView(withId(R.id.restaurant_name)).check(matches(isDisplayed()))
+    }
+    @Test
+    fun verifyThatImageIsVisible() {
+        onView(withId(R.id.login_later_button)).perform(click())
+        sleep(2000)
+        onView(withText("Ocean Seafood")).perform(click())
+        onView(withId(R.id.restaurant_image_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.restaurant_image_view)).check(matches(isEnabled()))
     }
 }
