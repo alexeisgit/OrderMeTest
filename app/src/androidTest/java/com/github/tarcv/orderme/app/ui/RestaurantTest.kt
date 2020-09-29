8f8ef9dfe888d7daace390bcbd4b3be5b014748a
Sergey Buzovsky <sbuzovsky@gmail.com>
2020-09-28 18:47:02 -0700

    Converted our existing test into Screen Objects.

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/RestaurantTest.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/RestaurantTest.kt
@@ -1,20 +1,16 @@
 package com.github.tarcv.orderme.app.ui
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import com.github.tarcv.orderme.app.R
+import com.github.tarcv.orderme.app.ui.Screens.LoginScreen
+import com.github.tarcv.orderme.app.ui.Screens.RestaurantListScreen
+import com.github.tarcv.orderme.app.ui.Screens.RestaurantScreen
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.lang.Thread.sleep
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class RestaurantTest {
@@ -25,26 +21,39 @@ class RestaurantTest {
 
     @Test
     fun verifyRestTitle() {
-        onView(withId(R.id.login_later_button)).perform(click())
+
+        val loginScreen = LoginScreen()
+
+        loginScreen.clickOnLoginLaterButton()
 
         sleep(2000)
 
-        onView(withText("Ocean Seafood")).perform(click())
+        val restaurantListScreen = RestaurantListScreen()
 
-        onView(withId(R.id.restaurant_name)).check(matches(isDisplayed()))
+        restaurantListScreen.selectRestaurant("Ocean Seafood")
+
+        val restaurantScreen = RestaurantScreen()
+
+        restaurantScreen.checkIfTitleIsDisplayed()
     }
 
     @Test
     fun verifyThatImageIsVisible() {
 
-        onView(withId(R.id.login_later_button)).perform(click())
+        val loginScreen = LoginScreen()
+
+        loginScreen.clickOnLoginLaterButton()
 
         sleep(2000)
 
-        onView(withText("Ocean Seafood")).perform(click())
+        val restaurantListScreen = RestaurantListScreen()
 
-        onView(withId(R.id.restaurant_image_view)).check(matches(isDisplayed()))
+        restaurantListScreen.selectRestaurant("Ocean Seafood")
 
-        onView(withId(R.id.restaurant_image_view)).check(matches(isEnabled()))
+        val restaurantScreen = RestaurantScreen()
+
+        restaurantScreen.checkIfImageIsDisplayed()
+
+        restaurantScreen.checkIfImageIsEnabled()
     }
 }
---
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Screens/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.github.tarcv.orderme.app.ui.Screens
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.github.tarcv.orderme.app.R
+
+class LoginScreen {
+
+    private val loginLaterButton = ViewMatchers.withId(R.id.login_later_button)
+
+    fun clickOnLoginLaterButton() {
+        onView(loginLaterButton).perform(ViewActions.click())
+    }
+}
